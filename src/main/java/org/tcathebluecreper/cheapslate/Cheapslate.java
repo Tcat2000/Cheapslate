5da8344f8f595d35f7d98c1ba8df2dc271cc3163
Taylor <taylordspivey@gmail.com>
2025-10-04 11:57:22 -0500

    Port to 1.21.1

--- src/main/java/org/tcathebluecreper/cheapslate/Cheapslate.java
+++ src/main/java/org/tcathebluecreper/cheapslate/Cheapslate.java
@@ -1,16 +1,19 @@
 package org.tcathebluecreper.cheapslate;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.item.*;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantments;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.player.PlayerEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import org.slf4j.Logger;
 
 @Mod(Cheapslate.MODID)
@@ -18,9 +21,9 @@ public class Cheapslate {
     public static final String MODID = "cheapslate";
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public Cheapslate() {
-        MinecraftForge.EVENT_BUS.register(this);
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    public Cheapslate(IEventBus modEventBus, ModContainer modContainer) {
+        NeoForge.EVENT_BUS.register(this);
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
     @SubscribeEvent
@@ -29,9 +32,9 @@ public class Cheapslate {
             ItemStack tool = event.getEntity().getMainHandItem();
             if(
                     tool.getTags().anyMatch(tag -> tag.location().equals(ResourceLocation.tryParse("cheapslate:fast_pickaxes"))) &&
-                    tool.getAllEnchantments().getOrDefault(Enchantments.BLOCK_EFFICIENCY,0) >= Config.efficiencyLevel &&
+                    tool.getEnchantments().getLevel(Minecraft.getInstance().level.registryAccess().lookupOrThrow(Registries.ENCHANTMENT).getOrThrow(Enchantments.EFFICIENCY)) >= Config.EFFICIENCY_LEVEL.get() &&
                         (event.getEntity().hasEffect(MobEffects.DIG_SPEED) &&
-                        event.getEntity().getEffect(MobEffects.DIG_SPEED).getAmplifier() >= Config.hasteLevel) || Config.hasteLevel == -1
+                        event.getEntity().getEffect(MobEffects.DIG_SPEED).getAmplifier() >= Config.HASTE_LEVEL.get()) || Config.HASTE_LEVEL.get() == -1
             ) event.setNewSpeed(10000);
         }
     }
--- src/main/java/org/tcathebluecreper/cheapslate/Config.java
+++ src/main/java/org/tcathebluecreper/cheapslate/Config.java
@@ -1,26 +1,12 @@
 package org.tcathebluecreper.cheapslate;
 
-import net.minecraftforge.common.ForgeConfigSpec;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.config.ModConfigEvent;
+import net.neoforged.neoforge.common.ModConfigSpec;
 
-@Mod.EventBusSubscriber(modid = Cheapslate.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class Config {
-    private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder().comment("Tools tagged with #cheapslate:fast_pickaxes will instant-mine blocks tagged with #cheapslate:fast_blocks when the following conditions are met (-1 = disabled condition)");
+    private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder().comment("Tools tagged with #cheapslate:fast_pickaxes will instant-mine blocks tagged with #cheapslate:fast_blocks when the following conditions are met (-1 = disabled condition)");
 
-    private static final ForgeConfigSpec.IntValue EFFICIENCY_LEVEL = BUILDER.comment("Efficiency level required for #cheapslate:fast_tools tools to mine #cheapslate:fast_blocks").defineInRange("efficiency", 5, -1, 255);
-    private static final ForgeConfigSpec.IntValue HASTE_LEVEL = BUILDER.comment("Haste level required for #cheapslate:fast_tools tools to mine #cheapslate:fast_blocks (haste 1 = level 0)").defineInRange("hast", 1, -1, 255);
+    public static final ModConfigSpec.IntValue EFFICIENCY_LEVEL = BUILDER.comment("Efficiency level required for #cheapslate:fast_tools tools to mine #cheapslate:fast_blocks").defineInRange("efficiency", 5, -1, 255);
+    public static final ModConfigSpec.IntValue HASTE_LEVEL = BUILDER.comment("Haste level required for #cheapslate:fast_tools tools to mine #cheapslate:fast_blocks (haste 1 = level 0)").defineInRange("hast", 1, -1, 255);
 
-    static final ForgeConfigSpec SPEC = BUILDER.build();
-
-    public static int efficiencyLevel;
-    public static int hasteLevel;
-
-
-    @SubscribeEvent
-    static void onLoad(final ModConfigEvent event) {
-        efficiencyLevel = EFFICIENCY_LEVEL.get();
-        hasteLevel = HASTE_LEVEL.get();
-    }
+    static final ModConfigSpec SPEC = BUILDER.build();
 }
